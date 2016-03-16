aa27b86430093cb2e94a0d662eaaeea31ef83ca9
LJWatson <tink@tink.uk>
2016-03-16 14:45:21 +0000

    Updated the vibration API demo

--- vibrate/js/vibrate.js
+++ vibrate/js/vibrate.js
@@ -1,40 +1,31 @@
-(function ()
-{
+(function() {
+    'use strict';
 
-	function doThis ()
-	{
-		
-		//Put JavaScript into strict mode.
-		'use strict';
-		
-		if ("vibrate" in navigator)
-		{
-			var vibrateOnce = function (e)
-			{
-				window.navigator.vibrate(500);
-			};
-				
-			var vibrateTwice = function (e)
-			{
-				window.navigator.vibrate([500, 500, 500]);
-			};
-			
-			var vibrateMore = function (e)
-			{
-				window.navigator.vibrate([500, 500, 500, 500, 500]);
-			};
-			
-			document.getElementById("v1").addEventListener("click", vibrateOnce);
-			document.getElementById("v2").addEventListener("click", vibrateTwice);
-			document.getElementById("v3").addEventListener("click", vibrateMore);
-		} else
-		{
-			alert("This browser does not support the Vibration API");
-		}
-	}
-	
-	document.addEventListener('DOMContentLoaded', function ()
-	{
-		doThis();
-	});
+    function init() {
+
+        if ("vibrate" in navigator) {
+
+            var vibrateOnce = function(e) {
+                window.navigator.vibrate(500);
+            };
+
+            var vibrateTwice = function(e) {
+                window.navigator.vibrate([500, 500, 500]);
+            };
+
+            var vibrateMore = function(e) {
+                window.navigator.vibrate([500, 500, 500, 500, 500]);
+            };
+
+            document.getElementById("v1").addEventListener("click", vibrateOnce);
+            document.getElementById("v2").addEventListener("click", vibrateTwice);
+            document.getElementById("v3").addEventListener("click", vibrateMore);
+        } else {
+            alert("This browser does not support the Vibration API");
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        init();
+    });
 })();
