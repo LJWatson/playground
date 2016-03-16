7b5885f66da428c797b54b7a8fac1392e4f6fcae
Ian Pouncey <ipouncey@Ians-MacBook-Pro.local>
2016-03-16 16:49:57 +0000

    Added vibration visualiser

--- vibrate/js/vibrate.js
+++ vibrate/js/vibrate.js
@@ -7,14 +7,17 @@
 
             var vibrateOnce = function(e) {
                 window.navigator.vibrate(500);
+                vibrateVisualiser(500);
             };
 
             var vibrateTwice = function(e) {
                 window.navigator.vibrate([500, 500, 500]);
+                vibrateVisualiser([500, 500, 500]);
             };
 
             var vibrateMore = function(e) {
                 window.navigator.vibrate([500, 500, 500, 500, 500]);
+                vibrateVisualiser([500, 500, 500, 500, 500]);
             };
 
             document.getElementById("v1").addEventListener("click", vibrateOnce);
@@ -28,4 +31,74 @@
     document.addEventListener('DOMContentLoaded', function() {
         init();
     });
+})();
+
+(function () {
+    'use strict';
+
+    var AudioContext = window.AudioContext || window.webkitAudioContext,
+        audioCtx = new AudioContext(),
+        oscillator,
+        oscillatorType = 'square',
+        oscillatorFrequency = 90,
+        gainNode,
+        audioPlaying = false,
+        visualToggleClass = 'vibrate-on';
+
+    window.vibrateVisualiser = function (vibratePattern, vibrate) {
+        if (!Array.isArray(vibratePattern)) {
+            vibratePattern = [vibratePattern];
+        }
+        var viualisers = document.querySelectorAll('[data-vibrate-visualiser]'),
+            viualiserI,
+            viualiserL = viualisers.length,
+            vibratePatternI,
+            vibratePatternL = vibratePattern.length,
+            timeElapsed;
+
+        if (vibratePattern[0] > 0) {
+            for (viualiserI = 0; viualiserI < viualiserL; viualiserI++) {
+                toggle(viualisers[viualiserI]);
+            }
+        }
+        
+        for (vibratePatternI = 0, timeElapsed = 0; vibratePatternI < vibratePatternL; vibratePatternI++) {
+            timeElapsed += vibratePattern[vibratePatternI];
+            for (viualiserI = 0; viualiserI < viualiserL; viualiserI++) {
+                window.setTimeout(toggle, timeElapsed, viualisers[viualiserI]);
+            }
+        }
+        
+        if (vibrate && "vibrate" in navigator) {
+            window.navigator.vibrate(vibratePattern);
+        }
+    };
+
+    function toggle (visualiser) {
+        visualToggle(visualiser);
+        audioToggle();
+    }
+
+    function visualToggle (visualiser) {
+        visualiser.classList.toggle(visualToggleClass);
+    }
+
+    function audioToggle () {
+        if (audioCtx) {
+            if (audioPlaying) {
+                oscillator.stop();
+            }
+            else {
+                oscillator = audioCtx.createOscillator();
+                gainNode = audioCtx.createGain();
+                oscillator.connect(gainNode);
+                gainNode.connect(audioCtx.destination);
+                oscillator.type = oscillatorType; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+                oscillator.frequency.value = oscillatorFrequency;
+                oscillator.start();
+            }
+            audioPlaying = !audioPlaying;
+        }
+    }
+
 })();
