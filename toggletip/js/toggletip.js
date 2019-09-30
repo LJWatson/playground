bf143d03c0fbfc7b7de6acaa02d9ffd8b692cc84
LJWatson <tink@tink.uk>
2019-09-30 20:59:03 +0100

    Adds a broken toggletip example

---
+++ toggletip/js/toggletip.js
@@ -0,0 +1,37 @@
+(function () {
+
+	var button = document.getElementById('button');
+	var tip = document.getElementById('tip');
+	var content = document.getElementById('content');
+
+	button.setAttribute('aria-expanded', 'false');
+	button.setAttribute('aria-describedby', 'tip');
+
+	tip.setAttribute('hidden', true);
+
+	content.setAttribute('role', 'tooltip');
+	content.setAttribute('aria-live', 'polite');
+
+	function toggleTip(e) {
+
+	  if (tip.hasAttribute('hidden')) {
+	    button.setAttribute('aria-expanded', 'true');
+	    content.innerHTML = "This information will answer your question... Press escape to close this tooltip.";
+	    tip.removeAttribute('hidden');
+		} else {
+	    button.setAttribute('aria-expanded', 'false');
+	    content.innerHTML = '';
+	    tip.setAttribute('hidden', true);
+		}
+	}
+
+	button.addEventListener('click', toggleTip, false);
+
+	document.addEventListener('keydown', function(e) {
+
+	  if (e.keyCode == 33) {
+	    toggleTip();
+	  }
+	});
+
+})();
