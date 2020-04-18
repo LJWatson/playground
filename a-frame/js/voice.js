e6d3d3c1360e083dcb158c3ca8d15853fcb21356
LJWatson <tink@tink.uk>
2020-04-18 16:46:08 +0100

    Adds WebXR Speech API experiment

--- a-frame/js/voice.js
+++ a-frame/js/voice.js
@@ -1,37 +1,72 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
-function init() {
+    function init() {
 
-if (window.SpeechSynthesisUtterance === undefined) {
-        alert("This browser does not support the Web Speech API");
-    } else {
+        if (window.SpeechSynthesisUtterance === undefined) {
+            alert("This browser does not support the Web Speech API");
+        } else {
 
-var allSceneElements = document.getElementById("scene").children;
-var sceneElements = Array.from(allSceneElements);
-sceneElements.splice(4, 6);
-var sceneElementsLength = sceneElements.length;
+            // Get all elements in the a-scene.
+            var allSceneElements = document.getElementById("scene").children;
+            var sceneElements = Array.from(allSceneElements);
 
-var sceneDesc = "The scene contains ";
+            // Get count of elements and splice to remove (3) non-visible elements.
+            var sceneElementsCount = sceneElements.length;
+            sceneElements.splice(sceneElementsCount - 3, sceneElementsCount);
+            var sceneElementsLength = sceneElements.length;
 
-var i;
-for (i = 0; i < sceneElementsLength -1; i++) {
-var name = sceneElements[i].tagName.slice(1);
-var colour = sceneElements[i].getAttribute("color");
- 
-sceneDesc += "a " +  colour + " " + name + ", ";
-}
+            // Set up scene description.
+            var sceneDesc = "The scene contains ";
 
-    sceneDesc += "and " + sceneElements[sceneElementsLength -1].getAttribute("color") + " " + sceneElements[sceneElementsLength -1].tagName.slice(1) + ".";
+            var i;
+            for (i = 0; i < sceneElementsLength; i++) {
 
-var utterance = new SpeechSynthesisUtterance();
-utterance.text = sceneDesc;
-window.speechSynthesis.speak(utterance);
-}
-}
+                // Get the element name; remove the a- and convert to lowercase.
+                var name = sceneElements[i].tagName.slice(2).toLowerCase();
 
-document.addEventListener("DOMContentLoaded", function() {
-    init();
-});
-    
+                // Get the element's colour.
+                var colour = sceneElements[i].getAttribute("color");
+
+                // Change element names to more descriptive form if necessary.
+                if (name === "box") {
+                    name = "cube";
+                }
+
+                if (name === "plane") {
+                    name = "ground";
+                }
+
+                if (i != sceneElements.length - 1) {
+
+                    // Check the name and change the preposition if necessary.
+                    if (name !== "sky" && name !== "ground") {
+                        sceneDesc += " a " + colour + " " + name + ",";
+                    }
+                    else {
+                        sceneDesc += " " + colour + " " + name + ",";
+                    }
+                }
+                else {
+                    if (name !== "sky" && name !== "ground") {
+                        sceneDesc += " and a " + colour + " " + name + ".";
+                    }
+                    else {
+                        sceneDesc += " and " + colour + " " + name + ".";
+                    }
+                }
+            }
+            console.log(sceneDesc);
+
+            // Create the speech object and content to speak.
+            var utterance = new SpeechSynthesisUtterance();
+            utterance.text = sceneDesc;
+            window.speechSynthesis.speak(utterance);
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+        init();
+    });
+
 })();
