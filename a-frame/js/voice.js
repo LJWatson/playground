63c7b01263f87ef0c7134a66c76b8cbea2c20ee8
LJWatson <tink@tink.uk>
2020-05-02 18:30:45 +0100

    Overwrites original to test for continuous listening

--- a-frame/js/voice.js
+++ a-frame/js/voice.js
@@ -1,72 +1,45 @@
 (function () {
-    'use strict';
+'use strict';
 
-    function init() {
+function init() {
 
-        if (window.SpeechSynthesisUtterance === undefined) {
-            alert("This browser does not support the Web Speech API");
-        } else {
+// Set recognition interface to SpeechRecognition regardless of browser.
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 
-            // Get all elements in the a-scene.
-            var allSceneElements = document.getElementById("scene").children;
-            var sceneElements = Array.from(allSceneElements);
+// Create SpeechRecognition and SpeechSynthesisUtterance objects.
+const recognition = new SpeechRecognition();
+const utterance = new SpeechSynthesisUtterance();
 
-            // Get count of elements and splice to remove (3) non-visible elements.
-            var sceneElementsCount = sceneElements.length;
-            sceneElements.splice(sceneElementsCount - 3, sceneElementsCount);
-            var sceneElementsLength = sceneElements.length;
+var button = document.getElementById("button");
+var display = document.getElementById("display");
 
-            // Set up scene description.
-            var sceneDesc = "The scene contains ";
+button.addEventListener("click", () => {
+    listen();
+console.log("Ready");
+});
 
-            var i;
-            for (i = 0; i < sceneElementsLength; i++) {
+function listen () {
+recognition.start();
+recognition.continuous = false;
+recognition.interimResults = false;
+console.log("Listening");
+}
 
-                // Get the element name; remove the a- and convert to lowercase.
-                var name = sceneElements[i].tagName.slice(2).toLowerCase();
+recognition.onresult = function(event) {
+var transcript = event.results[0][0].transcript;
+display.innerText = transcript;
+listen();
+}
 
-                // Get the element's colour.
-                var colour = sceneElements[i].getAttribute("color");
+recognition.onend = function() {
+    listen();
+};
 
-                // Change element names to more descriptive form if necessary.
-                if (name === "box") {
-                    name = "cube";
-                }
 
-                if (name === "plane") {
-                    name = "ground";
-                }
+}
 
-                if (i != sceneElements.length - 1) {
-
-                    // Check the name and change the preposition if necessary.
-                    if (name !== "sky" && name !== "ground") {
-                        sceneDesc += " a " + colour + " " + name + ",";
-                    }
-                    else {
-                        sceneDesc += " " + colour + " " + name + ",";
-                    }
-                }
-                else {
-                    if (name !== "sky" && name !== "ground") {
-                        sceneDesc += " and a " + colour + " " + name + ".";
-                    }
-                    else {
-                        sceneDesc += " and " + colour + " " + name + ".";
-                    }
-                }
-            }
-            console.log(sceneDesc);
-
-            // Create the speech object and content to speak.
-            var utterance = new SpeechSynthesisUtterance();
-            utterance.text = sceneDesc;
-            window.speechSynthesis.speak(utterance);
-        }
-    }
-
-    document.addEventListener("DOMContentLoaded", function () {
-        init();
-    });
+document.addEventListener("DOMContentLoaded", function () {
+init();
+});
 
 })();
